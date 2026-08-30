cee48a8f027063d2afee3b753c42c7392dfc0fbf
Andy Scott <theandiman@gmail.com>
2026-08-30 18:39:14 +0100

    fix(shared): add Min and Max validation annotations to Rating

--- src/main/java/com/recipe/shared/model/Rating.java
+++ src/main/java/com/recipe/shared/model/Rating.java
@@ -1,6 +1,8 @@
 package com.recipe.shared.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,6 +25,9 @@ public class Rating {
     private String userId;
     private String userName;
     private String userPhotoUrl;
+
+    @Min(1)
+    @Max(5)
     private Integer ratingValue;
     private String reviewText;
 
